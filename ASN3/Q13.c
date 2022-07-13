9b292802c4aad4c51ee3cd8d25ba3df854b7eb3e
Avinash Singh Gahlowt <63177028+Avinash-Gahlowt@users.noreply.github.com>
2022-07-13 19:44:17 +0530

    Files are named as Q2 for Question 2 and so on..

---
+++ ASN3/Q13.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter three digit number");
+    scanf("%d",&num);
+    printf("After rotation one digit towards right %d%d",num%10,num/10);
+}
