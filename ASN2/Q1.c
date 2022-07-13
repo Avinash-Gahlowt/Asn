46d36aeb82a4c17f4af03b1efb8eea37899925e4
Avinash Singh Gahlowt <63177028+Avinash-Gahlowt@users.noreply.github.com>
2022-07-13 20:04:29 +0530

    Update Q1.c

--- ASN2/Q1.c
+++ ASN2/Q1.c
@@ -1,14 +1,8 @@
 #include<stdio.h>
 int main(){
-   
-    int n1,n2,n3;
-    scanf("%d%d%d",&n1,&n2,&n3);
-    if (n1 >= n2 && n1 >= n3)
-        printf("The greates number is %d",n1);
-    else if (n2>=n1&&n2>=n3)
-        printf("The greatest number is %d",n2);
-    else
-        printf("The greatest number is %d",n3);
+    int num;
+    printf("Enter any Number to remove its last digit");
+    scanf("%d",&num);
+    printf("%d",num/10);
     return 0;
-    
 }
