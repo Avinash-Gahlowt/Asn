7b0a5d9ffd5c1cef3e60df1b104a8a406c41fe7d
Avinash Singh Gahlowt <63177028+Avinash-Gahlowt@users.noreply.github.com>
2022-07-13 20:04:50 +0530

    Update Q1.c

--- ASN2/Q1.c
+++ ASN2/Q1.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
     int num;
-    printf("Enter any Number to remove its last digit");
+    printf("Enter any number to print its unit digit");
     scanf("%d",&num);
-    printf("%d",num/10);
-    return 0;
+   printf("%d",num%10);
+   return 0;
 }
