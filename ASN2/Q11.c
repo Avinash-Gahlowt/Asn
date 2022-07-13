be0f2964b13f8138ea4a3bd7834cffdc12ce333e
Avinash Singh Gahlowt <63177028+Avinash-Gahlowt@users.noreply.github.com>
2022-07-13 20:05:31 +0530

    Update Q11.c

--- ASN2/Q11.c
+++ ASN2/Q11.c
@@ -1,11 +1,7 @@
 #include<stdio.h>
 int main(){
-    float s1,s2,s3,s4,s5;
-    printf("Enter 5 subjects marks");
-    scanf("%f%f%f%f%f",&s1,&s2,&s3,&s4,&s5);
-    if(s1>=33&&s2>=33&&s3>=33&&s4>=33&&s5>=33)
-        printf("Student has passed the examination");
-    else
-        printf("Student has failed the examination");
-    return 0;
+    int num,dig;
+    scanf("%d%d", &num,&dig);
+    printf("%d",num+dig);
+    return 0;   
 }
