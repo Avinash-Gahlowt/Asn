c4bced5b15ee308ce9eab50f3aea86a51c0a41a9
Avinash Singh Gahlowt <63177028+Avinash-Gahlowt@users.noreply.github.com>
2022-07-13 19:52:46 +0530

    Update Q10.c

--- ASN3/Q10.c
+++ ASN3/Q10.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 int main(){
-    int x,num;
-    printf("Enter any number to replace its last digit");
-    scanf("%d",&num);
-    x=num%10;
-    printf("New Number is %d",num-x);
+    float cp,sp,A;
+    printf("Please enter Cost price and Sell price respectively");
+    scanf("%f%f",&cp,&sp);
+    A=sp-cp;
+    if(A<0)
+        printf("Loss percentage is %f",((-A)*100)/cp);
+    else if (A==0)
+        printf("There is no loss and No profit");
+    else
+        printf("The profit percentage is %f",(A*100)/cp);
+  
     return 0;
 }
