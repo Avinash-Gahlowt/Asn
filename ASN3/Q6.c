407528e370fa40771e0031359ddb2f64603a3b40
Avinash Singh Gahlowt <63177028+Avinash-Gahlowt@users.noreply.github.com>
2022-07-13 19:57:23 +0530

    Update Q6.c

--- ASN3/Q6.c
+++ ASN3/Q6.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 int main(){
-    char c;
-    printf("Enter any character to get its ASCII Code");
-    scanf("%c",&c);
-    printf("%d",(int)c);
+   int number1,number2;
+   printf("Enter two numbers");
+   scanf("%d%d",&number1,&number2);
+    if(number1>number2||number1==number2)
+        printf("The greater number is %d",number1);
+    else
+        printf("The greater number is %d",number2);
     return 0;
 }
